bfe0feeba8aea21fa419631a47a34297b3491fa7
Marc Philipp <mail@marcphilipp.de>
2025-05-01 14:45:07 +0200

    Add solution to exercise 6

--- src/test/java/Exercise06_MathTests.java
+++ src/test/java/Exercise06_MathTests.java
@@ -1,16 +1,35 @@
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class Exercise06_MathTests {
 
     @ParameterizedTest(name = "sqrt({0}) = {1}")
     @CsvSource({
             " 1, 1.00",
-            " 2, 1.41"
+            " 2, 1.41",
+            " 4, 2.00",
     })
     void sqrt(double input, double expectedOutput) {
         assertEquals(expectedOutput, Math.sqrt(input), 0.01);
+    }
+
+    @ParameterizedTest(name = "floor({0}) = {1}")
+    @MethodSource
+    void floor(double input, int expectedOutput) {
+        assertEquals(expectedOutput, Math.floor(input), 0.01);
+    }
+
+    static Stream<Arguments> floor() {
+        return Stream.of(
+                arguments(+1.1d, +1),
+                arguments(-1.1d, -2)
+        );
     }
 }
