10de755534af9175d1f79b9579ade7be30bcecb1
Marc Philipp <mail@marcphilipp.de>
2025-05-01 14:57:37 +0200

    Use Store's resource management

--- src/test/java/ExecutorServiceExtension.java
+++ src/test/java/ExecutorServiceExtension.java
@@ -1,6 +1,7 @@
-import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
+import org.junit.jupiter.api.extension.ExtensionContext.Store.CloseableResource;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.support.TypeBasedParameterResolver;
@@ -11,24 +12,29 @@ import java.util.concurrent.Executors;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ExecutorServiceExtension extends TypeBasedParameterResolver<ExecutorService> implements BeforeAllCallback, AfterAllCallback {
+class ExecutorServiceExtension extends TypeBasedParameterResolver<ExecutorService> implements BeforeAllCallback {
 
-    private ExecutorService executorService;
-
-    @Override
-    public void beforeAll(ExtensionContext context) throws Exception {
-        executorService = Executors.newCachedThreadPool();
-    }
+    private static final Namespace NAMESPACE = Namespace.create(ExecutorServiceExtension.class);
 
     @Override
-    public void afterAll(ExtensionContext context) throws Exception {
-        executorService.shutdownNow();
-        boolean terminated = executorService.awaitTermination(5, SECONDS);
-        assertTrue(terminated, "ExecutorService did not shut down within timeout of 5 seconds");
+    public void beforeAll(ExtensionContext context) {
+        context.getStore(NAMESPACE)
+                .put(ExecutorServiceResource.class, new ExecutorServiceResource(Executors.newCachedThreadPool()));
     }
 
     @Override
     public ExecutorService resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-        return executorService;
+        return extensionContext.getStore(NAMESPACE)
+                .get(ExecutorServiceResource.class, ExecutorServiceResource.class)
+                .executorService();
+    }
+
+    private record ExecutorServiceResource(ExecutorService executorService) implements CloseableResource {
+        @Override
+        public void close() throws Throwable {
+            executorService.shutdownNow();
+            boolean terminated = executorService.awaitTermination(5, SECONDS);
+            assertTrue(terminated, "ExecutorService did not shut down within timeout of 5 seconds");
+        }
     }
 }
